ba10d9ce94d93be801344371815416f35153c10e
seondal <sseondal@gmail.com>
2024-11-08 12:49:56 +0900

    ✨ feat :  검색창 및 카테고리 선택 창

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,3 +1,37 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Page() {
-  return <>asdf</>;
+  const [searchActive, setSearchActive] = useState(false);
+  const [categoryActive, setCategoryActive] = useState(false);
+
+  return (
+    <>
+      <fieldset className="grid">
+        <input
+          name="search"
+          type="search"
+          placeholder="키워드로 검색하기"
+          onFocus={() => {
+            setCategoryActive(false);
+            setSearchActive(true);
+          }}
+          onBlur={() => setSearchActive(false)}
+        />
+        <label>
+          <input
+            name="terms"
+            type="checkbox"
+            role="switch"
+            checked={categoryActive}
+            onChange={() => setCategoryActive((cur) => !cur)}
+          />
+          카테고리
+        </label>
+      </fieldset>
+      {searchActive && <div className="bg-black">검색창</div>}
+      {categoryActive && <div>카테고리창</div>}
+    </>
+  );
 }
