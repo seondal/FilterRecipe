af107da2de0d2c3de04b7bbc7480bc04dbaf1cd0
seondal <sseondal@gmail.com>
2024-11-15 06:40:56 +0900

    ✨ feat :  레시피 간단하ㅔㄱ 보여줌

--- src/utils/transform.ts
+++ src/utils/transform.ts
@@ -7,7 +7,7 @@ export function RecipeDataForCard(data: RecipeI): RecipeCardDataI {
   const recipe = PROPERTIES.map((prop) => ({
     property: prop.name,
     value: propertyData[prop.key],
-  }));
+  })).filter((item) => item.value !== 0);
 
   return {
     ...data,
