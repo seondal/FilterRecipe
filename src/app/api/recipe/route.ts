fa9827a3814f4ce5dd8b71839af2dd58f49406a4
seondal <sseondal@gmail.com>
2024-11-15 02:53:35 +0900

    🌟 feat :  에러 발생하면 홈화면으로 리다이렉션

--- src/app/api/recipe/route.ts
+++ src/app/api/recipe/route.ts
@@ -11,6 +11,7 @@ import { myApi } from "../instance";
 import { RecipeI } from "@/interface/recipe";
 import { SITE } from "@/constants/env";
 
+/** GET Recipe */
 export async function GET(req: NextRequest, res: NextResponse) {
   const recipeSnapshot = await getDocs(collection(db, "recipe"));
   const results = recipeSnapshot.docs.map((doc) => doc.data());
@@ -18,55 +19,62 @@ export async function GET(req: NextRequest, res: NextResponse) {
   return NextResponse.json(results);
 }
 
+/** POST Recipe */
 export async function POST(req: NextRequest, res: NextResponse) {
-  const body = await req.formData();
+  try {
+    const body = await req.formData();
 
-  // 보정 전 이미지 업로드
-  const beforeImgbbRes = await myApi.post(`/imgbb/beforeImage`, body);
-  const beforeImgData = beforeImgbbRes.data;
+    // 이미지 업로드
+    const [beforeImgbbRes, afterImgbbRes] = await Promise.all([
+      myApi.post(`/imgbb/beforeImage`, body),
+      myApi.post(`/imgbb/afterImage`, body),
+    ]);
 
-  // 보정 후 이미지 업로드
-  const afterImgbbRes = await myApi.post(`/imgbb/afterImage`, body);
-  const afterImgData = afterImgbbRes.data;
+    const beforeImgData = beforeImgbbRes.data;
+    const afterImgData = afterImgbbRes.data;
 
-  const data = {
-    title: body.get("title") as string,
-    image: {
-      before: beforeImgData.url,
-      after: afterImgData.url,
-    },
-    category: {
-      main: body.get("mainCategory") as string,
-      sub: body.get("subCategory") as string,
-    },
-    description: body.get("description") as string,
-    property: {
-      exposure: parseInt(body.get("exposure") as string),
-      brightness: parseFloat(body.get("brightness") as string),
-      highlight: parseFloat(body.get("highlight") as string),
-      shadow: parseFloat(body.get("shadow") as string),
-      contrast: parseFloat(body.get("contrast") as string),
-      lightness: parseFloat(body.get("lightness") as string),
-      blackPoint: parseFloat(body.get("blackPoint") as string),
-      saturation: parseFloat(body.get("saturation") as string),
-      colorClarity: parseFloat(body.get("colorClarity") as string),
-      warmth: parseFloat(body.get("warmth") as string),
-      hue: parseFloat(body.get("hue") as string),
-      sharpness: parseFloat(body.get("sharpness") as string),
-      clarity: parseFloat(body.get("clarity") as string),
-      noiseReduction: parseFloat(body.get("noiseReduction") as string),
-      vignette: parseFloat(body.get("vignette") as string),
-    },
-  };
+    const data = {
+      title: body.get("title") as string,
+      image: {
+        before: beforeImgData.url,
+        after: afterImgData.url,
+      },
+      category: {
+        main: body.get("mainCategory") as string,
+        sub: body.get("subCategory") as string,
+      },
+      description: body.get("description") as string,
+      property: {
+        exposure: parseInt(body.get("exposure") as string),
+        brightness: parseFloat(body.get("brightness") as string),
+        highlight: parseFloat(body.get("highlight") as string),
+        shadow: parseFloat(body.get("shadow") as string),
+        contrast: parseFloat(body.get("contrast") as string),
+        lightness: parseFloat(body.get("lightness") as string),
+        blackPoint: parseFloat(body.get("blackPoint") as string),
+        saturation: parseFloat(body.get("saturation") as string),
+        colorClarity: parseFloat(body.get("colorClarity") as string),
+        warmth: parseFloat(body.get("warmth") as string),
+        hue: parseFloat(body.get("hue") as string),
+        sharpness: parseFloat(body.get("sharpness") as string),
+        clarity: parseFloat(body.get("clarity") as string),
+        noiseReduction: parseFloat(body.get("noiseReduction") as string),
+        vignette: parseFloat(body.get("vignette") as string),
+      },
+    };
 
-  const newRef = doc(collection(db, "recipe"));
-  const newDoc: RecipeI = {
-    id: newRef.id,
-    ...data,
-    timestamp: serverTimestamp(),
-  };
-  await setDoc(newRef, newDoc);
+    const newRef = doc(collection(db, "recipe"));
+    const newDoc: RecipeI = {
+      id: newRef.id,
+      ...data,
+      timestamp: serverTimestamp(),
+    };
+    await setDoc(newRef, newDoc);
 
-  const redirectUrl = `${SITE}/recipe/${newRef.id}`;
-  return NextResponse.redirect(redirectUrl);
+    const redirectUrl = `${SITE}/recipe/${newRef.id}`;
+    return NextResponse.redirect(redirectUrl);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.redirect("/");
+  }
 }
