2e91051f21636a977b82e283aab352b98093f592
seondal <sseondal@gmail.com>
2024-11-15 15:23:32 +0900

    ✨ feat :  마이페이지 내가 업로드한 레시피 보기

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -5,34 +5,45 @@ import { myApi } from "../api/instance";
 import { RecipeI } from "@/interface/recipe";
 import { RecipeDataForCard } from "@/utils/transform";
 import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
 
 export default function Page() {
-  const [data, setData] = useState<RecipeI[]>();
+  const { data } = useSession();
+  const [recipeData, setRecipeData] = useState<RecipeI[]>();
 
   async function fetchFeed() {
-    const res = await myApi.get<RecipeI[]>("/recipe", {});
-    setData(res.data);
+    const res = await myApi.get<RecipeI[]>("/recipe", {
+      params: {
+        userid: data?.user.id,
+      },
+    });
+    setRecipeData(res.data);
   }
 
   useEffect(() => {
     fetchFeed();
-  }, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data?.user.id]);
 
   if (!data) {
-    return <div aria-busy="true" />;
+    return <h3>로그인하고 레시피를 공유해보세요</h3>;
   }
 
-  if (data.length === 0) {
+  if (!recipeData) {
+    return <article aria-busy="true" />;
+  }
+
+  if (recipeData.length === 0) {
     return <h3>올린 레시피가 없어요</h3>;
   }
 
   return (
     <>
-      <div className="flex flex-wrap gap-y-3 gap-x-1">
-        {data.map((item, idx) => (
+      <article className="flex flex-wrap gap-y-3 gap-x-1">
+        {recipeData.map((item, idx) => (
           <RecipeSmallCard data={RecipeDataForCard(item)} key={idx} />
         ))}
-      </div>
+      </article>
     </>
   );
 }
