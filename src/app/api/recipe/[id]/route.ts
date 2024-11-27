f2ba3affb166583158001ea426eafe34349352c3
seondal <sseondal@gmail.com>
2024-11-27 16:16:06 +0900

    📄 docs :  레시피 관련 api 주석 추가

--- src/app/api/recipe/[id]/route.ts
+++ src/app/api/recipe/[id]/route.ts
@@ -3,6 +3,28 @@ import { ParamsWithIdI } from "@/interface/page";
 import { doc, getDoc } from "firebase/firestore";
 import { NextRequest, NextResponse } from "next/server";
 
+/**
+ * @swagger
+ * /recipe/{id}:
+ *   get:
+ *     summary: 레시피 상세보기
+ *     description: 레시피 아이디 파라미터를 기반으로 레시피 정보를 반환합니다
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         description: 레시피 아이디 (레시피 상세보기 경로와 동일)
+ *         required: true
+ *         schema:
+ *           type: string
+ *           example: "OCewCpg7SCVJhUJQ4w6o"
+ *     responses:
+ *       200:
+ *         description: 레시피 정보
+ *       404:
+ *         description: 주어진 아이디에 해당하는 레시피가 없음
+ *       500:
+ *         description: Internal server error
+ */
 export async function GET(req: NextRequest, { params }: ParamsWithIdI) {
   const ref = doc(db, "recipe", params.id);
 
